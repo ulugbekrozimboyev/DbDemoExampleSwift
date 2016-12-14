6b5b571b50b26f9a4eb8fa61c68bf14824f4ca27
Ulug'bek <ulugbekrozimboyev@gmail.com>
2016-12-14 09:51:30 +0500

    init porject

---
+++ DbDemoExampleSwift/StudentTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  StudentTableViewCell.swift
+//  DbDemoExampleSwift
+//
+//  Created by Ulugbek on 12/14/16.
+//  Copyright © 2016 Ulugbek. All rights reserved.
+//
+
+import UIKit
+
+class StudentTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lblContent: UILabel!
+    
+    @IBOutlet weak var btnEdit: UIButton!
+    
+    @IBOutlet weak var btnDelete: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ DbDemoExampleSwift/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  DbDemoExampleSwift
+//
+//  Created by Ulugbek on 12/14/16.
+//  Copyright © 2016 Ulugbek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var txtName: UITextField!
+
+    @IBOutlet weak var txtMarks: UITextField!
+    
+    @IBAction func btnSaveClicked(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var btnSaveClicked: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
